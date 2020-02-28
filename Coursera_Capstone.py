8e04af91185ab414ab5dee1e7d701fbe5f911096
cklooi96 <61548936+cklooi96@users.noreply.github.com>
2020-02-28 22:37:52 +0800

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Applied Data Science Capstone
+
+# #### This notebook will be mainly used for IBM Applied Data Science Capstone Project.
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
